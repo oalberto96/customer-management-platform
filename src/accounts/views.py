2503d97c86df1d6f2392e6691c3441719b17bc33
oalberto96 <ochoapublic@gmail.com>
2020-02-22 12:17:03 -0800

    finish dashboard

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,12 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product, Order, Customer
+
+def get_customers_orders():
+    customers = Customer.objects.all()
+    customers_orders_count = []
+    for customer in customers:
+        customers_orders_count.append({
+            'customer': customer,
+            'orders_count': customer.order_set.all().count()
+        })
+    return customers_orders_count
+    
 
 # Create your views here.
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    context = {
+        'last_5_orders': orders[:5],
+        'customers_orders': get_customers_orders(),
+        'total_orders': orders.count(),
+        'orders_delivered': orders.filter(status='Delivered').count(),
+        'orders_pending': orders.filter(status='Pending').count()
+    }
+    return render(request, 'accounts/dashboard.html', context)
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+    return render(request, 'accounts/products.html', context)
 
 def customer(request):
     return render(request, 'accounts/customer.html')
