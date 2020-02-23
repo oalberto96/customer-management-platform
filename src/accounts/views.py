2171503569dba6908b1a4b120f6343d61b106227
oalberto96 <ochoapublic@gmail.com>
2020-02-22 17:23:06 -0800

    add inline formset

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
 from .models import Product, Order, Customer
 from .forms import OrderForm
 
@@ -44,14 +45,22 @@ def customer(request, pk):
     }
     return render(request, 'accounts/customer.html', context)
 
-def create_order(request):
-    form = OrderForm()
+def create_order(request, pk):
+    OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status'), extra=2)
+    customer = Customer.objects.get(id=pk)
+    formset = OrderFormSet(
+        queryset=Order.objects.none(),
+        instance=customer,
+    )
+    # form = OrderForm(initial={
+    #     'customer': customer
+    # })
     if request.method == 'POST':
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            form.save()
+        formset = OrderFormSet(request.POST, instance=customer)
+        if formset.is_valid():
+            formset.save()
             return redirect('home')
-    context = {'form': form}
+    context = {'formset': formset}
     return render(request, 'accounts/order_form.html', context)
 
 def update_order(request, pk):
