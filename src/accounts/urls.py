6a3b5728faaed2f597e5102183f18afae1ca43c9
oalberto96 <ochoapublic@gmail.com>
2020-02-24 21:08:36 -0800

    add register and login

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('login/', views.login_page, name="login"),
+    path('register/', views.register_page, name="register"),
     path('products/', views.products, name="products"),
     path('customer/<int:pk>/', views.customer, name="customer"),
     path('create_order/<int:pk>', views.create_order, name="create_order"),
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from .models import Product, Order, Customer
-from .forms import OrderForm
+from .forms import OrderForm, CreateUserForm
 from .filters import OrderFilter
 
+def login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('home')
+    context = {}
+    return render(request, 'accounts/login.html', context)
+
+def register_page(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+            return redirect('login')
+    context = {'form': form}
+    return render(request, 'accounts/register.html', context)
 
 def get_customers_orders():
     customers = Customer.objects.all()
